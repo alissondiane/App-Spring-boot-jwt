2a6c5c8a5d55394a11615934b8b0becef50cbfb0
Alisson <diane_10_96@hotmail.com>
2020-03-24 23:20:48 -0500

    Add JWT

---
+++ src/main/java/com/alisson/springboot/datajpa/app/auth/SimpleGrantedAuthorityMixin.java
@@ -0,0 +1,9 @@
+package com.alisson.springboot.datajpa.app.auth;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public abstract class SimpleGrantedAuthorityMixin {
+	@JsonCreator
+	public SimpleGrantedAuthorityMixin(@JsonProperty("authority") String role) {}
+}
